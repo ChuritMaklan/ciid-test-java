3a5b896983e8f95806d6a36a340447297e9955a3
Anton <cuklinmarat@gmail.com>
2025-02-27 15:51:20 +0300

    fix db schema

--- bd/script.sql
+++ bd/script.sql
@@ -1,24 +1,21 @@
-CREATE TABLE client_type
+CREATE TABLE client_types
 (
     type_id SERIAL PRIMARY KEY,
     type_name VARCHAR(50) NOT NULL UNIQUE
 );
 
--- Insert types into clientType table
-INSERT INTO client_type (type_name) VALUES ('customer'), ('supplier');
+INSERT INTO client_types (type_name) VALUES ('customer'), ('supplier');
 
--- Create person table
-CREATE TABLE person
+CREATE TABLE clients
 (
-    person_id SERIAL PRIMARY KEY,
+    client_id SERIAL PRIMARY KEY,
     name      VARCHAR(100) NOT NULL,
     email     VARCHAR(100) UNIQUE,
     phone     VARCHAR(20),
     type_id   INTEGER NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES client_type (type_id) ON DELETE CASCADE
+    FOREIGN KEY (type_id) REFERENCES client_types (type_id) ON DELETE CASCADE
 );
 
--- Create parts table
 CREATE TABLE parts
 (
     part_id           SERIAL PRIMARY KEY,
@@ -27,17 +24,15 @@ CREATE TABLE parts
     price             INTEGER NOT NULL,
     quantity_in_stock INTEGER NOT NULL,
     description       VARCHAR(255),
-    FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES clients (client_id) ON DELETE CASCADE
 );
 
--- Create categories table
 CREATE TABLE categories
 (
     category_id SERIAL PRIMARY KEY,
     name        VARCHAR(50) NOT NULL UNIQUE
 );
 
--- Create part_categories table
 CREATE TABLE part_categories
 (
     part_id     INTEGER NOT NULL,
@@ -47,16 +42,14 @@ CREATE TABLE part_categories
     FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE
 );
 
--- Create orders table
 CREATE TABLE orders
 (
     order_id   SERIAL PRIMARY KEY,
     person_id  INTEGER,
     order_date DATE,
-    FOREIGN KEY (person_id) REFERENCES person (person_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES clients (client_id) ON DELETE CASCADE
 );
 
--- Create order_items table
 CREATE TABLE order_items
 (
     order_item_id SERIAL PRIMARY KEY,
