035561855a3cd92992f9833788c4f68f5b15c9f8
Anton <cuklinmarat@gmail.com>
2025-02-27 16:19:43 +0300

    fix db naming

--- bd/script.sql
+++ bd/script.sql
@@ -1,62 +1,69 @@
-CREATE TABLE client_types
+CREATE TABLE client_type
 (
-    type_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     type_name VARCHAR(50) NOT NULL UNIQUE
 );
 
-INSERT INTO client_types (type_name) VALUES ('customer'), ('supplier');
+-- Insert types into clientType table
+INSERT INTO client_type (type_name) VALUES ('customer'), ('supplier');
 
-CREATE TABLE clients
+-- Create person table
+CREATE TABLE person
 (
-    client_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name      VARCHAR(100) NOT NULL,
     email     VARCHAR(100) UNIQUE,
     phone     VARCHAR(20),
     type_id   INTEGER NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES client_types (type_id) ON DELETE CASCADE
+    FOREIGN KEY (type_id) REFERENCES client_type (id) ON DELETE CASCADE
 );
 
-CREATE TABLE parts
+-- Create parts table
+CREATE TABLE part
 (
-    part_id           SERIAL PRIMARY KEY,
+    id           SERIAL PRIMARY KEY,
     name              VARCHAR(255) NOT NULL,
     person_id         INTEGER,
     price             INTEGER NOT NULL,
     quantity_in_stock INTEGER NOT NULL,
     description       VARCHAR(255),
-    FOREIGN KEY (person_id) REFERENCES clients (client_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 
-CREATE TABLE categories
+-- Create categories table
+CREATE TABLE category
 (
-    category_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name        VARCHAR(50) NOT NULL UNIQUE
 );
 
-CREATE TABLE part_categories
+-- Create part_categories table
+CREATE TABLE part_category
 (
     part_id     INTEGER NOT NULL,
     category_id INTEGER NOT NULL,
     PRIMARY KEY (part_id, category_id),
-    FOREIGN KEY (part_id) REFERENCES parts (part_id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories (category_id) ON DELETE CASCADE
+    FOREIGN KEY (part_id) REFERENCES part(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
 );
 
-CREATE TABLE orders
+-- Create orders table
+CREATE TABLE "order"
 (
-    order_id   SERIAL PRIMARY KEY,
+    id   SERIAL PRIMARY KEY,
     person_id  INTEGER,
     order_date DATE,
-    FOREIGN KEY (person_id) REFERENCES clients (client_id) ON DELETE CASCADE
+    FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
 );
 
-CREATE TABLE order_items
+-- Create order_items table
+CREATE TABLE order_item
 (
-    order_item_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     order_id      INTEGER,
     part_id       INTEGER,
     quantity      INTEGER NOT NULL,
     price         INTEGER NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders (order_id) ON DELETE CASCADE,
-    FOREIGN KEY (part_id) REFERENCES parts (part_id) ON DELETE CASCADE
+    FOREIGN KEY (order_id) REFERENCES "order" (id) ON DELETE CASCADE,
+    FOREIGN KEY (part_id) REFERENCES part (id) ON DELETE CASCADE
 );
